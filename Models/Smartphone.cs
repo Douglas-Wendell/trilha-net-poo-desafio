56847887d60f9c4e46f5a336ec320280f790f7e2
Douglas Wendell <douglaswendell530@gmail.com>
2024-10-10 16:23:17 -0300

    segundo commit

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -8,9 +8,12 @@ namespace DesafioPOO.Models
         public int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 Console.WriteLine("Smartphone Nokia:");
-Smartphone nokia = new Nokia(numero: "12345678");
+Smartphone nokia = new Nokia(numero: "12345678", modelo: "modelo 1", imei:"222222222", 64);
 nokia.Ligar();
 nokia.InstalarAplicativo("Whatsapp");
 
-Console.WriteLine("Smartphone iPhoe:");
-Smartphone iphone = new Iphone (numero: "87654321");
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone (numero: "87654321", modelo: "modelo 2", imei:"111111111", 128);
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("Telegram");
