b66c12a25c1aec389cbc9d6d27eb0a4b0e0d3e2b
Douglas Wendell <douglaswendell530@gmail.com>
2024-10-10 15:47:24 -0300

    Desafio entregue

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "12345678");
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone iPhoe:");
+Smartphone iphone = new Iphone (numero: "87654321");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
